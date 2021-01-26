f4d083d39eb40962d25b4235b56a0d6d6d9a373b
Marcel Hoekstra <marcel.hoekstra@cito.nl>
2021-01-26 16:18:57 +0100

    corrected urls

--- frontend/environments/environment.nobackend.dev.ts
+++ frontend/environments/environment.nobackend.dev.ts
@@ -3,5 +3,5 @@ export const environment = {
   apibase: 'http://localhost:5000/', // localhost is replace by 10.0.2.2 if onCordova = true
   useBackend: false,
   updateUrl:
-    'https://citolab-diggel2.azurewebsites.net/remote_server_apk/version.xml',
+    'https://diggel.azurewebsites.net/remote_server_apk/version.xml',
 };
--- frontend/environments/environment.prod.ts
+++ frontend/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apibase: 'https://citolab-diggel2.azurewebsites.net/', // value will be reduced to / if its serve on save url
+  apibase: 'https://diggel.azurewebsites.net/', // value will be reduced to / if its serve on save url
   useBackend: true,
   updateUrl:
-    'https://citolab-diggel2.azurewebsites.net/remote_server_apk/version.xml',
+    'https://diggel.azurewebsites.net/remote_server_apk/version.xml',
 };
